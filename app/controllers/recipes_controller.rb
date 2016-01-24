4491bb8291357858a0f9fa1c19586695e77eeaad
Lukeghenco <lukeghenco@gmail.com>
2016-01-24 01:34:43 -0500

    Done.

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,37 @@
+class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+    @ingredients = Ingredient.all
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipes_path
+  end
+
+  def edit
+    find_recipe
+    @ingredients = Ingredient.all
+    @recipe_ingredients = @recipe.ingredients
+  end
+
+  def update
+    find_recipe
+    @recipe.update(recipe_params)
+    @recipe.save
+    redirect_to recipes_path
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:name, ingredient_ids: [])
+    end
+
+    def find_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+end
