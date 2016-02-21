8ea2ef7acbf79b703a4a946908fe076d92f49513
Chen Yang <yangc5@mail.uc.edu>
2016-02-21 22:33:40 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+recipe1 = Recipe.create(name: "french 1")
+recipe2 = Recipe.create(name: "french 2")
+recipe3 = Recipe.create(name: "french 3")
+
+ingredient1 = Ingredient.create(name: "salt")
+ingredient2 = Ingredient.create(name: "pepper")
+ingredient3 = Ingredient.create(name: "flour")
