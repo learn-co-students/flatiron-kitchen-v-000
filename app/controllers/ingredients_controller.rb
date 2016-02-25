ed728c25d94c879898956c32e4a5a928b9a530c8
Rawradin <edgar.rawr@gmail.com>
2016-02-25 11:16:18 -0800

    Done.

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,33 @@
+class IngredientsController < ApplicationController
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def index
+    @ingredients = Ingredient.all  
+  end
+
+  def create
+    @ingredient = Ingredient.create(ingredient_params)
+    redirect_to ingredient_path(@ingredient)
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.update(ingredient_params)
+    redirect_to ingredient_path(@ingredient)
+  end
+
+  private
+    def ingredient_params
+      params.require(:ingredient).permit(:name)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 FlatironKitchen::Application.routes.draw do
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +55,6 @@ FlatironKitchen::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :ingredients
+  resources :recipes
 end
