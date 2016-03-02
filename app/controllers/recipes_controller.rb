28c57ebda624e339aef5f0e5b88293304541fc7f
Adam King <adam.king0126@gmail.com>
2016-03-02 11:24:26 -0600

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,36 @@
+require 'pry'
 class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to @recipe
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to @recipe
+  end
+
+  private
+    
+    def recipe_params
+      params.require(:recipe).permit(:name, :ingredient_ids => [])
+    end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,13 @@ class Recipe < ActiveRecord::Base
   validates :name, presence: true
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients  
+
+  def ingredient_ids=(ingredients)
+    self.ingredients = []
+    ingredients.delete_if { |ingredient| ingredient == "" }
+    ingredients.each do |ingredient|
+      self.ingredients << Ingredient.find(ingredient)
+    end
+  end
+
 end
