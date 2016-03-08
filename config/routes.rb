47b1d280f40b1a1433b8d99c1fa5250f0c79e531
empireofryan <rjohnson2001@gmail.com>
2016-03-07 22:15:59 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 FlatironKitchen::Application.routes.draw do
+  resources :recipes
+
+  resources :recipe_ingredients
+
+  resources :ingredients
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ test/controllers/recipe_ingredients_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RecipeIngredientsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
