4aa11c4cb22476004c11781fc1f631fd397eaa10
shinyrachel <rachel.kory@gmail.com>
2016-04-04 10:37:54 -0400

    complete

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,36 @@
+class RecipesController < ApplicationController
+
+  before_action :set_recipe, only: [:show, :edit, :update]
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :ingredient_ids => [])
+  end
+
+end
