1659640805c6ca81adab2e0b84b5bf7546ebcdae
jaeku <ja.ku@rutgers.edu>
2016-04-06 01:04:16 -0400

    Done.

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,33 @@
+class RecipesController < ApplicationController
+  def index
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.create(recipes_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipes_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+
+  def recipes_params
+    params.require(:recipe).permit(:name, :ingredient_ids => [])
+  end
+end
