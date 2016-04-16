239597101c45492583789d04c00b0e9e7e104163
cernanb <cernanb@gmail.com>
2016-04-16 00:47:59 -0700

    created flatiron kitchen recipe app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 FlatironKitchen::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  resources :ingredients, :recipes
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
@@ -39,7 +39,7 @@ FlatironKitchen::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
