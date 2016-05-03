e4816770917ff622cfd01acd8f932b14e46f829e
corya0687 <corya0687@gmail.com>
2016-05-03 18:09:35 -0400

    Done.

---
+++ app/models/recipe.rb
@@ -0,0 +1,20 @@
+class Recipe < ActiveRecord::Base
+  validates :name, presence: true
+
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+
+  def ingredients_ids=(ingredients)
+    ingredients.each do |i|
+      if !i.blank?
+        ingredient = Ingredient.find(i)
+        self.ingredients << ingredient
+      end
+    end
+  end
+
+  def ingredients_ids
+    self.ingredients
+  end
+
+end
