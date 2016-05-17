003eb166491d36a65456c6516fe138e4bae3ba6c
mhnguyen289 <mhnguyen289@gmail.com>
2016-05-16 22:09:03 -0700

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,48 +1,39 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update]
+ 
+   def show
+   end
+ 
+   def new
+     @recipe = Recipe.new
+   end
 
-	before_action :set_recipe, only: [:show,:edit, :update]
-
-	def new 
-		@recipe = Recipe.new
-		@ingredients = Ingredient.all
-	end
-
-	def index
-		@recipes = Recipe.all
-	end
-
-	def show
-		@recipe = Recipe.find(params[:id])
-	end
-
-	def create
-		@recipe = Recipe.new(recipe_params)
-		if @recipe.save
-			redirect_to recipe_path(@recipe)
-		else
-			render :new
-		end
-	end
-
-	def edit
-		
-	end
-
-	def update
-		if @recipe.update(recipe_params)
-			redirect_to recipe_path(@recipe)
-		else
-			render :edit
-		end
-	end
-
-	private
-
-	def set_recipe
-		@recipe = Recipe.find(params[:id])
-	end
-
-	def recipe_params
-		params.require(:recipe).permit(:name, :ingredients_ids => [])
-	end
+   def create
+     @recipe = Recipe.new(recipe_params)
+     if @recipe.save
+       redirect_to recipe_path(@recipe)
+     else
+       render :new
+     end
+   end
+ 
+   def edit
+   end
+   
+   def update
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+   end
+ 
+ 
+   private
+     def set_recipe
+       @recipe = Recipe.find(params[:id])
+     end
+     def recipe_params
+       params.require(:recipe).permit(:name, :ingredient_ids => [])
+     end
 end
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,4 +1,4 @@
 class RecipeIngredient < ActiveRecord::Base
-	belongs_to :recipe
-	belongs_to :ingredient
+  belongs_to :recipe
+  belongs_to :ingredient
 end
