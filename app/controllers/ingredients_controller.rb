b2fc5c8dddf55eac2c828beda13917698ea0bda2
theRingleman <sam.ringleman@gmail.com>
2016-06-20 17:30:29 -0600

    Done.

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,32 @@
+class IngredientsController < ApplicationController
+
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    ingredient = Ingredient.create(ingredient_params)
+    redirect_to ingredients_path
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.update(ingredient_params)
+    redirect_to ingredients_path
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+
+end
