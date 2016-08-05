2f8b614cd73de8ba7afccf9b9d6c426e0b68f7a4
Saul Ocampo <digitalh2o@gmail.com>
2016-08-05 16:39:50 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -33,7 +33,7 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name)
+    params.require(:recipe).permit(:name, ingredient_ids: [])
   end
 
 end
