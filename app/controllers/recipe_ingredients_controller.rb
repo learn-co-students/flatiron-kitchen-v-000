c1f559ff27d7a99a6d13a64e2ba4bb9f72ceaa63
jchu4483 <jchu4483@gmail.com>
2016-08-14 17:53:56 -0400

    Done.

---
+++ app/controllers/recipe_ingredients_controller.rb
@@ -0,0 +1,25 @@
+class RecipeIngredientsController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+end
