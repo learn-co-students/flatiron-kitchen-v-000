945120f322ac3f9cd822b3b0b942bc4e3e02fdc5
jwolfe890 <jwolfe890@gmail.com>
2016-08-30 04:10:12 -0400

    Done.

---
+++ app/models/recipe.rb
@@ -0,0 +1,10 @@
+class Recipe < ActiveRecord::Base
+
+has_many :ingredients, through: :recipe_ingredients 
+has_many :recipe_ingredients
+
+validates :name, presence: true
+
+ accepts_nested_attributes_for :ingredients
+
+end
