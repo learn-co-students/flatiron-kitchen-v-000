3a661b31392f220388f807e159d9ec356cc1902a
Caleb Faught <faughtcj@gmail.com>
2016-09-23 16:00:38 +0000

    Done.

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,33 @@
+class IngredientsController < ApplicationController
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def new
+    @ingredient = Ingredient.new()
+  end
+
+  def create
+    @ingredient = Ingredient.create(ingredient_params)
+    redirect_to ingredient_path(@ingredient)
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.update(ingredient_params)
+    redirect_to ingredient_path(@ingredient)
+  end
+
+  private
+  def ingredient_params
+    params.require("ingredient").permit(:name)
+  end
+end
