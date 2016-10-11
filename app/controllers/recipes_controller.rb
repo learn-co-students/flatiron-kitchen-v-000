3434ab6c91a0a565238e190ffc644c12585f47a8
Lenn Hypolite <lenntriniman@hotmail.com>
2016-10-11 20:39:40 +0000

    Done.

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,38 @@
+  class RecipesController < ApplicationController
+    def new
+      @recipe = Recipe.new
+    end
+
+    def index
+      @recipes = Recipe.all
+    end
+
+    def edit
+       @recipe = Recipe.find(params[:id])
+    end
+
+    def update
+      @recipe = Recipe.find(params[:id])
+      if @recipe.update(recipe_params)
+        redirect_to recipes_path
+      else
+        render :edit
+      end
+    end
+
+    def create
+      @recipe = Recipe.new(recipe_params)
+      if @recipe.save
+        redirect_to recipes_path
+      else
+        render :new
+      end
+    end
+
+    private
+      def recipe_params
+        params.require(:recipe).permit(:name, :ingredient_ids=>[], :ingredients_attributes=>[:name])
+      end
+      # , :content, :tag_ids => [], tags_attributes: [:name]
+
+end
---
+++ app/models/recipe_ingredient.rb
@@ -0,0 +1,5 @@
+class RecipeIngredient < ActiveRecord::Base
+  belongs_to :recipes
+  belongs_to :ingredients
+
+end
