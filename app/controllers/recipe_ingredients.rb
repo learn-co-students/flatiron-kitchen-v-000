87406cb3878471a4f969bde1db2f6d9721f0bd81
DownsideupmediaUSA <brandondecarlo@live.com>
2016-10-14 14:01:31 -0400

    Done.

---
+++ app/controllers/recipe_ingredients.rb
@@ -0,0 +1,2 @@
+class RecipeIngredients < ApplicationController
+end
---
+++ app/models/recipe.rb
@@ -0,0 +1,14 @@
+class Recipe < ActiveRecord::Base
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+
+  validates_presence_of :name
+  # validates :name, presence: true
+
+  # def ingredient_ids=(ingredient_ids)
+  #   ingredients = Ingredient.where("id in(?)", ingredient_ids)
+  #   self.ingredients = ingredients
+  # end
+
+
+end
