235ce7e9bbbb827af46b021168cb51074cdaa3cf
bmolina-nyc <bruce.d.molina@gmail.com>
2016-11-12 14:50:09 -0500

    Done.

---
+++ NOTES.md
@@ -0,0 +1,5 @@
+1. I need an ingredients resource/MVC ( ingredients have many recipies )
+2. I need a recipies resource/MVC  ( recipies have many ingredients )
+3. I need an recipie_ingredients Model and Table (join table - belongs to ingredients and recipies)
+
+4. Need to create routes for my pages (new, edit, index, show)
---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,57 @@
+class IngredientsController < ApplicationController
+
+ 
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+      if @ingredient.save
+        redirect_to @ingredient
+      else
+        render 'new'
+      end
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update 
+    @ingredient = Ingredient.find(params[:id])
+
+    @ingredient.update(ingredient_params)
+    redirect_to @ingredient 
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def index
+    @ingredients = Ingredient.all
+  end
+  
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+
+
+end
+
+
+
+# // ♥ rake routes
+#          Prefix Verb URI Pattern                     Controller#Action
+#         recipes GET  /recipes(.:format)              recipes#index
+#      new_recipe GET  /recipes/new(.:format)          recipes#new
+#     edit_recipe GET  /recipes/:id/edit(.:format)     recipes#edit
+#          recipe GET  /recipes/:id(.:format)          recipes#show
+#     ingredients GET  /ingredients(.:format)          ingredients#index
+#  new_ingredient GET  /ingredients/new(.:format)      ingredients#new
+# edit_ingredient GET  /ingredients/:id/edit(.:format) ingredients#edit
+#      ingredient GET  /ingredients/:id(.:format)      ingredients#show
