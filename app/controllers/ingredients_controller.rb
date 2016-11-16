51f901f7b331e3d4729f579453bbb6e8aa6c2e4a
AliceKB <alicekristinabalbuena@gmail.com>
2016-11-16 05:49:14 -0500

    Done.

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,33 @@
+class IngredientsController < ApplicationController
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    @ingredient.save
+
+    redirect_to ingredient_path(@ingredient)
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.update(ingredient_params)
+
+    redirect_to ingredient_path(@ingredient)
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+end
---
+++ app/models/recipe.rb
@@ -0,0 +1,6 @@
+class Recipe < ActiveRecord::Base
+  validates :name, presence: true
+
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+end
