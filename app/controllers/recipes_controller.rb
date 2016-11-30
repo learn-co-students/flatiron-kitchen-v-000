c2646c9aba2d9aca1420c1c3f9ee963a356b0b93
Joel <joelbitar1986@gmail.com>
2016-11-30 17:01:33 +0100

    Done.

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,34 @@
+class RecipesController < ApplicationController
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to @recipe
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, ingredient_ids:[], ingredients_attributes: [:name])
+  end
+
+end
