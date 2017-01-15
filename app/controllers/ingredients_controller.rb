c6a7e1f70f5413549a247bd1b2572d2db1785259
Adam Friedl <ajfriedl@gmail.com>
2017-01-15 17:13:04 -0500

    Done.

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,41 @@
+class IngredientsController < ApplicationController
+
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.find_or_create_by(ingredient_params)
+    if @ingredient.save
+      redirect_to ingredients_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    ingredient = Ingredient.find(params[:id])
+    ingredient.update(ingredient_params)
+
+    redirect_to ingredients_path
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :quantity)
+  end
+
+end
---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,41 @@
+class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    # if @recipe.save
+    #   redirect_to recipe_path(@recipe)
+    # else
+    #   render 'new'
+    redirect_to recipe_path(@recipe)
+    # end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :ingredient_ids => [], :ingredients_attributes => [:name])
+  end
+end
