881e4f56fbee4d9629859c4828591ec2ac29529e
Adam Friedl <ajfriedl@gmail.com>
2017-01-15 17:15:56 -0500

    Done.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,5 +2,5 @@ class Ingredient < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
 
-  validates :name, :quantity presence: true
+  validates :name, presence: true
 end
