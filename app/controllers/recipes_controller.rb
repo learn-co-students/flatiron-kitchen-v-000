427bcef19db75ffbcba9dd314759c33e2db8baa7
Adam Cooper <amcooper@gmail.com>
2017-02-08 13:54:42 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,7 @@ class RecipesController < ApplicationController
 	def index
 		@recipes = Recipe.all
 	end
-	
+
 	def new
 		@recipe = Recipe.new
 		@ingredients = Ingredient.all
@@ -18,9 +18,20 @@ class RecipesController < ApplicationController
   	@recipe = Recipe.find(params[:id])
   end
 
+  def edit
+  	@recipe = Recipe.find(params[:id])
+  	@ingredients = Ingredient.all
+  end
+
+  def update
+  	recipe = Recipe.find(params[:id])
+  	recipe.update(recipe_params)
+  	redirect_to recipes_path
+  end
+
   private
 
     def recipe_params
-    	params.require(:recipe).permit(:name)
+    	params.require(:recipe).permit(:name, ingredient_ids: [])
     end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,14 +1,11 @@
 class Recipe < ActiveRecord::Base
 	has_many :recipe_ingredients
 	has_many :ingredients, through: :recipe_ingredients
-	accepts_nested_attributes_for :ingredients
 
 	validates :name, presence: true
 
-	def ingredients_attributes=(ingredient_attributes)
-		ingredient_attributes.values.each do |ingredient_attribute|
-			ingredient = Ingredient.find_or_create_by(ingredient_attribute)
-			self.ingredients << ingredient
-		end
+	def ingredient_ids=(ingredient_ids)
+		ingredients = Ingredient.where("id in (?)", ingredient_ids)
+		self.ingredients = ingredients
 	end
 end
