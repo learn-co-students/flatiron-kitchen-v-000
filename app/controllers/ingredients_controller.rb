0d0cb59f2d6515d2c4303e3a1d571db39c3ef611
Malind Kowalski <malind.nikulski@gmail.com>
2017-02-10 13:13:00 -0700

    completed flatiron kitchen

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,44 @@
+class IngredientsController < ApplicationController
+  before_action :select_ingredient, only: [:show, :edit, :update]
+  layout "ingredient"
+
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def show
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      redirect_to @ingredient
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @ingredient.update(ingredient_params)
+      redirect_to @ingredient
+    else
+      render :edit
+    end
+  end
+
+  private
+  def select_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+end
