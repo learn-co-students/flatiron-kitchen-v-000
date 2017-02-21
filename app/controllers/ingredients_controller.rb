5db838289f7430a60b714561f36614ee61c84c45
Jon Fougner <jon.fougner@gmail.com>
2017-02-21 22:40:06 +0000

    Done.

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+class IngredientsController < ApplicationController
+
+  def new
+    # binding.pry
+    @ingredient = Ingredient.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
