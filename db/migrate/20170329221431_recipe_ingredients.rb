4a8af04dc299527eb40d04480d1ffe86a4026fda
github username pianistps <github email address pianistps@yahoo.com>
2017-03-31 16:44:05 -0500

    added join table

--- db/migrate/20170329221431_recipe_ingredients.rb
+++ db/migrate/20170329221431_recipe_ingredients.rb
@@ -1,4 +1,9 @@
 class RecipeIngredients < ActiveRecord::Migration
   def change
+    create_table :recipe_ingredients do |t|
+      t.integer "recipe_id"
+      t.integer "ingredient_id"
+
+      t.timestamps null: false
   end
 end
