b65c336287153fd48a808309ed9e821ab401eaf3
github username pianistps <github email address pianistps@yahoo.com>
2017-04-03 11:36:08 -0500

    added create action and strong params, added show action and template

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -4,4 +4,23 @@ class IngredientsController < ApplicationController
     @ingredient = Ingredient.new
   end
 
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      redirect_to @ingredient
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+
 end
