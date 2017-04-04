1e1e134b14e0c8e2f61ba014661969ae5dd654a0
John Pfingst <jpfingst@outlook.com>
2017-04-04 11:19:53 +0000

    all tests pass

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,36 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def create
+    @recipe = Recipe.create(recipe_params)
+    if !@recipe.nil?
+      redirect_to @recipe
+    else
+      render :new
+    end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
   def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render :edit
+    end
+  end
+
+private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :ingredient_ids => [])
   end
 
 end
