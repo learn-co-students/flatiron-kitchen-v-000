3cdfd80e34aeea47668f02bdcf2e090ce353251d
github username pianistps <github email address pianistps@yahoo.com>
2017-04-04 11:38:55 -0500

    added create action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,21 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render 'new'
+    end
+  end
+
   def edit
   end
 
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name)
+  end
 end
