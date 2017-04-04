ca656d6e78134742f33d49c97324bfbf2c2dc841
github username pianistps <github email address pianistps@yahoo.com>
2017-04-04 13:25:36 -0500

    added pry, added checkboxes for ingredients and edited create for ingredients in recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -23,6 +23,6 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name)
+    params.require(:recipe).permit(:name, :ingredient_ids => [])
   end
 end
