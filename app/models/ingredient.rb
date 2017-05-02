35b0ce455339de64863b3d2209f1b7d0501bf8ea
Howard DeVennish <howarddevennish@gmail.com>
2017-05-02 21:50:21 +0000

    Done.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,6 @@ class Ingredient < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
   validates :name, presence: true
-  validates :name, uniqueness: true
+  # validates :name, uniqueness: true
 
 end
