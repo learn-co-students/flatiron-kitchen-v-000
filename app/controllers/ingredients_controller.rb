b43c44ae2cfed3c9bfc0cbb482c1408040d7fb17
anytimesoon <damien@delartigue.co.uk>
2017-05-09 21:04:13 +0100

    Done.

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,30 @@
+class IngredientsController < ApplicationController
+	def show
+		@ingredient = Ingredient.find(params[:id])
+	end
+
+	def new
+		@ingredient = Ingredient.new
+	end
+
+	def create
+		ingredient = Ingredient.create(ingredient_params)
+		redirect_to ingredient_url(ingredient)
+	end
+
+	def edit
+		@ingredient = Ingredient.find(params[:id])
+	end
+
+	def update
+		ingredient = Ingredient.find(params[:id])
+		ingredient.update(ingredient_params)
+		redirect_to ingredient_url(ingredient)
+	end
+
+	private
+
+		def ingredient_params
+			params.require(:ingredient).permit(:name)
+		end
+end
