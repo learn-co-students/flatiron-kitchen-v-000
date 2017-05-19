bbeec64d33bc9177f27675c967b0d734c89ec0c0
Hima Chitalia <hima_chhag@yahoo.com>
2017-05-19 16:22:48 +0000

    Done.

---
+++ app/models/recipe.rb
@@ -0,0 +1,9 @@
+class Recipe < ActiveRecord::Base
+
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+  validates_presence_of :name
+
+  accepts_nested_attributes_for :ingredients, reject_if: lambda {|attributes| attributes['name'].blank?}
+
+end
---
+++ app/views/recipe_ingredients/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @recipe_ingredient, :id, :recipe_id, :ingredient_id, :created_at, :updated_at
