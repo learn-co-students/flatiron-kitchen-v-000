36a356f6c14da1e06d13db6ef86e1ca978e3852f
Menachem Hornbacher <me@mendel.tech>
2017-06-11 19:49:18 +0000

    Done.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,30 @@
 class IngredientsController < ApplicationController
+
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def edit
+    @ingredient = Ingredient.find_by(id: params[:id])
+  end
+
+  def create
+    ing = Ingredient.create(post_params)
+    ing.save ? redirect_to(ingredients_path) : render(:new)
+  end
+
+  def update
+    ingredient = Ingredient.find_by(id: params[:id])
+    ingredient.update(post_params)
+    ingredient.save ? redirect_to(ingredients_path) : render(:edit)
+  end
+
+  private
+  def post_params
+    params.require(:ingredient).permit(:name)
+  end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,37 @@
 class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    load_instance
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def edit
+    load_instance
+  end
+
+  def create
+    @recipe = Recipe.new(post_params)
+    @recipe.save ? redirect_to(recipe_path(@recipe)) : render(:new)
+  end
+
+  def update
+    load_instance
+    @recipe.update(post_params)
+    @recipe.save ? redirect_to(recipe_path(@recipe)) : render(:edit)
+  end
+
+  private
+  def load_instance
+    @recipe = Recipe.find_by(id: params[:id])
+  end
+  def post_params
+    params.require(:recipe).permit(:name, ingredient_ids: [])
+  end
 end
