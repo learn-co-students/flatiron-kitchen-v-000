3a0176e1942704c1c4cd42e8fe8b93b8f9a04f65
lukeymac <lukey.mac@outlook.com>
2017-06-11 22:30:56 -0400

    Done.

---
+++ app/models/recipe.rb
@@ -0,0 +1,33 @@
+class Recipe < ActiveRecord::Base
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+
+  validates_presence_of :name
+  accepts_nested_attributes_for :ingredients, reject_if: proc {|a| a[:name].blank?}
+
+  def total_makeable
+    self.recipe_ingredients.map do |recipe_ingredient|
+      if recipe_ingredient.quantity && recipe_ingredient.quantity > 0
+        recipe_ingredient.ingredient.inventory_total / recipe_ingredient.quantity
+      else
+        0
+      end
+    end.min
+  end
+
+  def recipe_ingredient_attrs=(ri_attrs)
+    ri_attrs.keep_if { |ria| ria[:ingredient_id].present? }
+
+    self.recipe_ingredients.destroy_all
+    ri_attrs.each do |attrs|
+      self.recipe_ingredients.build(attrs)
+    end
+
+    self.save
+  end
+
+  def quantity_for(ingredient)
+    ri = self.recipe_ingredients.where(ingredient: ingredient).first
+    ri.quantity if ri
+  end
+end
