43ce7c6c50b05ad1e70e20968fa188a843e0cba4
Jdville03 <jarrel.delottinville@gmail.com>
2017-07-11 23:35:48 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,15 +6,4 @@ class Recipe < ActiveRecord::Base
 
   accepts_nested_attributes_for :ingredients, reject_if: proc { |attributes| attributes['name'].blank? }
 
-  # def ingredients_attributes=(ingredient_attributes)
-  #   ingredient_attributes.values.each do |ingredient_attribute|
-  #     if ingredient_attributes[:name].present?
-  #       ingredient = Ingredient.find_or_create_by(ingredient_attribute)
-  #       if !self.ingredients.include?(ingredient)
-  #         self.recipe_ingredients.build(ingredient: ingredient)
-  #       end
-  #     end
-  #   end
-  # end
-
 end
