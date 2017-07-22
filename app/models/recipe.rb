4c7e15e4846a3003f832f1c31d273f67db4c5244
Niki Lee <ew1415048nk@gmail.com>
2017-07-23 00:14:19 +0900

    Done.

---
+++ app/models/recipe.rb
@@ -0,0 +1,11 @@
+class Recipe < ActiveRecord::Base
+  validates :name, presence: true
+
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+  accepts_nested_attributes_for :ingredients, reject_if: proc { |attributes| attributes['name'].blank? }
+
+  # def ingredients=(ingredients)
+  #
+  # end
+end
