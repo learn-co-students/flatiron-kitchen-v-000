c7541e784360ff1a08a3767f4d15816bc85e8f10
Will-Sauer <will@heueso.com>
2017-07-25 14:35:42 -0700

    models passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 FlatironKitchen::Application.routes.draw do
+
+  resources :recipes
+
+  resources :ingredients
+
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +60,3 @@ FlatironKitchen::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
