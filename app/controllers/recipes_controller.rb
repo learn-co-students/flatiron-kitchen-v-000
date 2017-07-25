8bacb3a6a0032946886b869c713c0119349b97ec
Will-Sauer <will@heueso.com>
2017-07-25 15:56:04 -0700

    finished

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,36 @@
 class RecipesController < ApplicationController
+before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+  def new
+    @recipe = Recipe.new
+  
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end  
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @ingredient = @recipe.ingredients.build
+  end
+
+
+    private
+    def recipe_params
+      params.require(:recipe).permit(:name, ingredient_ids:[], ingredients_attributes: [:name])
+    end
+
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+
+
 end
