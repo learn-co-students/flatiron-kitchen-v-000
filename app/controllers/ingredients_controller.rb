3548ed400c6c87aab19122b1bf33f2ec95a9b5e9
Margarita Khodzka <mkhodzka@gmail.com>
2017-08-29 18:11:50 +0000

    added associations, validations, create action for ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,25 @@
 class IngredientsController < ApplicationController
+
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.find_or_create_by(ingredient_params)
+  end
+
+  def show
+
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit([:name])
+  end
+
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,25 @@
 class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+  end
+
+  def show
+    @ingredients = @recipe.ingredients
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit([:name])
+  end
+
 end
