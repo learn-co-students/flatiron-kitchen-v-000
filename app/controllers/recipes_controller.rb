253738b01eede038929d48f8b34801c38145400e
John Fewell <fewell@gmail.com>
2017-08-29 15:51:45 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -69,6 +69,6 @@ class RecipesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def recipe_params
-      params.require(:recipe).permit(:name)
+      params.require(:recipe).permit(:name, :ingredient_ids => [], )
     end
 end
