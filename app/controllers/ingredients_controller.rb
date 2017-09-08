4015325af3580e5dd806a4c2a7432e0e12aab67c
Andrew <github email address>
2017-09-08 19:27:11 -0400

    Done.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,12 +1,38 @@
 class IngredientsController < ApplicationController
 
   def index
+    @ingredients = Ingredient.all
+  end
 
+  def new
+    @ingredient = Ingredient.new
+
+  end
+  
+  def create
+    @ingredient = Ingredient.create(ingredients_params)
+    render "show"
+  end
+  
+  def show
+    @ingredient = Ingredient.find(params[:id])
+
+  end
+  def edit 
+    @ingredient = Ingredient.find(params[:id])
+    
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.update(ingredients_params)
+    render "show"
   end
 
 
   private
     def ingredients_params
-    
+      params.require(:ingredient).permit(:name)
+        
     end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,38 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
 
   end
 
-  def ingredient_ids=(ingredient_ids)
+  def new
+    @recipe = Recipe.new
+  end
+  def create
+    @recipe = Recipe.new(recipe_params)
+    
+    render "show"
+  end
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
 
   end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+  end
+
+  # def ingredient_ids=(ingredient_ids)
+
+  # end
 
   private
 
     def recipe_params
-      params.require()
+      params.require(:recipe).permit(:name)
     end
 end
