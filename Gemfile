fae669c6764aa4f6fc678fbe69655cd8ecb2783a
Terry Blue <terryblue@comcast.net>
2017-09-13 21:38:45 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ group :test do
   gem 'rspec-collection_matchers', '~> 1.1.2'
   gem 'rspec-rails', '~> 3.3.3'
   gem 'capybara', '~> 2.5.0'
+  gem "pry", '~> 0.10.3'
 end
 
 # Use ActiveModel has_secure_password
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,36 @@
 class RecipesController < ApplicationController
+before_action :set_recipe, only: [:show, :update, :edit, :destroy]
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+  end
+
+  def new
+    @recipe = Recipe.new
+    # add two empty ingredients to fill out
+    4.times do
+     @recipe.ingredients.build
+    end
+  end
+
+  def create
+    binding.pry
+    @recipe = Recipe.create(recipe_params)
+    binding.pry
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    # modify method to accept the params hash keys
+    params.require(:recipe).permit(:name, ingredient_ids:[], ingredients_attributes: [:name, :quantity])
+  end
 end
