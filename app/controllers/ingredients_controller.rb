ccc7939964f337190758a837d517f0bcb3d3dc1b
Aimee Carney <acarney29@gmail.com>
2017-09-14 00:45:26 +0000

    Done.

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,45 @@
+require 'pry'
+
+class IngredientsController < ApplicationController
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      redirect_to @ingredient
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.update(ingredient_params)
+    redirect_to @ingredient
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def index
+
+  end
+
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+
+
+
+end
