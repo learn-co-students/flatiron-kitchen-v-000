4a355b0c6862f8c13df91ff74cd6c24013688211
Parv Kumar <parvathikumar92@gmail.com>
2017-10-05 19:13:05 +0000

    generated resources for recipes/ingredients

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 FlatironKitchen::Application.routes.draw do
+  resources :ingredients
+
+  resources :models
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
