8715dd026cf2f2faa10df6fe09ac0bf39fd42b59
Parv Kumar <parvathikumar92@gmail.com>
2017-10-06 01:21:58 +0000

    finished specs for recipes_controller

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ActiveRecord::Base
-  validates :name, presence: true 
+  validates :name, presence: true
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
 end
