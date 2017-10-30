35da6783ba6d467d988f1c7681797b5a4a52f741
aisayo <aisayo@icloud.com>
2017-10-30 00:41:22 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -30,6 +30,6 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name)
+    params.require(:recipe).permit(:name, ingredient_ids: [])
   end
 end
