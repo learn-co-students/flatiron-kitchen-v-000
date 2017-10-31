bedfb6c7afa9ffa21f6e764487787366fcdc9e38
Villy Siu <villysiu@gmail.com>
2017-10-31 22:34:11 +0000

    Done.

---
+++ db/migrate/20171031205947_add_column_to_recipe_ingredients.rb
@@ -0,0 +1,6 @@
+class AddColumnToRecipeIngredients < ActiveRecord::Migration
+  def change
+    add_column :recipe_ingredients, :recipe_id, :integer
+    add_column :recipe_ingredients, :ingredient_id, :integer
+  end
+end
