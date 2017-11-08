e67e4995d74751008026926f00c5098fbfb68e82
lpassamano <leighayna.josele@gmail.com>
2017-11-07 21:58:12 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -31,6 +31,6 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name)
+    params.require(:recipe).permit(:name, :ingredients => [])
   end
 end
