cd53863adb9f1d5383acb334b8254a22aaa2667e
lpassamano <leighayna.josele@gmail.com>
2017-11-09 13:11:07 -0500

    added notes for additional feature test - tracking quantity of ingredients

---
+++ spec/features/quantity/tracking_ingredients_quantity_spec.rb
@@ -0,0 +1,8 @@
+# 1. test for column in recipe_ingredients for quantity of ingredient
+# 2. add column into ingredients table for unit
+# 3. add to Recipe show page "You can make x batches of this recipe with the current amount of ingredients"
+# 4. update ingredients add/edit form to include unit
+# 5. update recipe add/edit form to include unit and how much of each ingredient/unit needed for recipe
+# 6. add column to ingredients table for stock (show how much total of ingredient we have in pantry)
+# 7. update ingredients add/edit form to include area for stock/quantity in pantry
+# 8. verify that the recipe show page is showing correct ammount of batches that can be made with the amounts of ingredients on hand
