536ab84f377f2e1be81bf0e59443fcc8e2c4a887
lpassamano <leighayna.josele@gmail.com>
2017-11-09 14:03:41 -0500

    updated seed file

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient < ActiveRecord::Base
   validates :name, presence: true
-  validates :unit, presence: true 
+  validates :unit, presence: true
 
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,30 @@
 # http://www.foodnetwork.com/recipes/trisha-yearwood/blueberry-pancakes-recipe.html
 
 blueberry_pancakes = Recipe.new(name: 'Blueberry Pancakes')
-blueberry_pancakes_ingredients = ['All-purpose flour', 'Sugar', 'Baking powder', 'Baking soda', 'Salt', 'Large eggs', 'Milk', 'Sour cream', 'Butter', 'Vanilla extract', 'Blueberries', 'Lemon zest']
-blueberry_pancakes_ingredients.each{|i| blueberry_pancakes.ingredients.build(name: i)}
+blueberry_pancakes_ingredients = [
+  {name: 'All-purpose flour', unit: 'cup'},
+  {name: 'Sugar', unit: "tablespoon"},
+  {name: 'Baking powder', unit: "teaspoon"},
+  {name: 'Baking soda', unit: "teaspoon"},
+  {name: 'Salt', unit: "teaspoon"},
+  {name: 'Large eggs', unit: "whole"},
+  {name: 'Milk', unit: "cup"},
+  {name: 'Sour cream', unit: "cup"},
+  {name: 'Butter', unit: "stick"},
+  {name: 'Vanilla extract', unit: "teaspoon"},
+  {name: 'Blueberries', unit: "cup"},
+  {name: 'Lemon zest', unit: "teaspoon"}
+]
+blueberry_pancakes_ingredients.each{|i| blueberry_pancakes.ingredients.build(i)}
 blueberry_pancakes.save
 
 # Jake the Dog's Bacon Pancakes Recipe
 # https://www.youtube.com/watch?v=1eO5U_uN7DQ
 
 bacon_pancakes = Recipe.new(name: 'Bacon Pancakes')
-bacon_pancakes_ingredients = ['Some bacon', 'Pancake']
-bacon_pancakes_ingredients.each{|i| bacon_pancakes.ingredients.build(name: i)}
+bacon_pancakes_ingredients = [
+  {name: 'Some bacon', unit: "strips"},
+  {name: 'Pancake', unit: "stack"}
+]
+bacon_pancakes_ingredients.each{|i| bacon_pancakes.ingredients.build(i)}
 bacon_pancakes.save
