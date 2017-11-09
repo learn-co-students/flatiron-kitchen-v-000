9b4c23ac3d37b5232c96e00fdcfe1a57a6f517ed
lpassamano <leighayna.josele@gmail.com>
2017-11-09 13:26:20 -0500

    updated Ingredient table and added unit validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108020908) do
+ActiveRecord::Schema.define(version: 20171109182231) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "unit"
+    t.integer  "stock"
   end
 
   create_table "recipe_ingredients", force: :cascade do |t|
