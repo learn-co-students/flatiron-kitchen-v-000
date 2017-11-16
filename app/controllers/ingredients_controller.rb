fdb7eaf713c6e88a985160904a09770b279a8da8
Annette Maza <nettymaza@Annettes-MacBook.local>
2017-11-16 15:45:37 -0700

    Done.

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,49 @@
+class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:show, :edit, :update]
+
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def show
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.valid?
+      @ingredient.save
+      redirect_to ingredients_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @ingredient.update(ingredient_params)
+
+    if @ingredient.save
+      redirect_to ingredients_path
+    else
+      render :edit
+    end
+
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+end
