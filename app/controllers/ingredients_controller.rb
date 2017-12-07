192e17751dffd7ab369e37c9b379dbf3ada8aa93
chrisjamesr <chrisjames.r@gmail.com>
2017-12-07 13:30:14 -0500

    passing create & edit ingredients & ingredient/recipe invalid with blank name

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,50 @@
+class IngredientsController < ApplicationController
+  before_action :set_ingredient!, only: [:show, :edit, :update, :delete]
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      redirect_to ingredient_path(@ingredient)
+    else
+      render :new 
+    end   
+  end
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @ingredient.update(ingredient_params)
+      redirect_to ingredient_path(@ingredient)
+    else
+      render :edit
+    end
+  end
+
+  def delete
+
+  end
+
+  private
+
+    def set_ingredient!
+      @ingredient = Ingredient.find(params[:id])
+    end
+
+    def ingredient_params
+      params.require(:ingredient).permit(:name)
+    end
+
+end  #  End of Class
---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,36 @@
+class RecipesController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+
+  private
+
+    def recipe_params
+
+    end
+end  #  End of Class
