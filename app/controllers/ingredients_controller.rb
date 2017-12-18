6b1e19bd8cdeb326a914720ee0f64ec52413774b
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-18 23:19:31 +0000

    Done.

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,38 @@
+class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:show, :edit, :update]
+
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def show
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      render 'show'
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @ingredient.update(ingredient_params)
+    render 'show'
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
+end
