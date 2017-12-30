7f778708cb9b0f7d5745e7d9e2c2dcb3aa52b84c
bkhartmire <bkhartmire@gmail.com>
2017-12-29 16:30:38 -0800

    Done.

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,38 @@
+class RecipesController < ApplicationController
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to @recipe
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name)
+  end
+end
