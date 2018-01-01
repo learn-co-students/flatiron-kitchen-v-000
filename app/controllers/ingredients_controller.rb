4f62dc696d466e396c615396fff8c913664c9f11
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-01-01 17:58:41 +0000

    Done.

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,32 @@
+class IngredientsController < ApplicationController
+    
+    def new 
+        @ingredient = Ingredient.new
+    end 
+    
+    def create
+         @ingredient = Ingredient.new(ingredient_params)
+         if @ingredient.save
+             redirect_to @ingredient
+         else 
+             render 'new'
+         end 
+        
+    end 
+    
+    def show
+        @ingredient = Ingredient.find(params[:id])
+    end 
+    
+    def edit
+        
+    end 
+    
+    def update 
+    end 
+    
+    def ingredient_params
+        params.require(:ingredient).permit(:name)
+    end 
+    
+end
