4b137e4a0da22671e7f7539bab514c1c354b21b5
Justin Tooley <jstooley823@gmail.com>
2018-01-04 19:22:21 +0000

    added controller methods

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,19 @@
 class IngredientsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,19 @@
 class RecipesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
 end
