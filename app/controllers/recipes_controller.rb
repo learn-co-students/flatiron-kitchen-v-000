b6fb71def0a8f2530119bfc85431d30ff055e90e
Jessie Huff <huffjd1@gmail.com>
2018-01-12 16:21:45 +0000

    Done.

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,33 @@
+class RecipesController < ApplicationController
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to @recipe
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, ingredient_ids: [])
+  end
+end
