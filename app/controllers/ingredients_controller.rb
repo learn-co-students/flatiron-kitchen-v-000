a4ec85c19b7c5b29ce6c0c260cdea6a787c608e3
Jacqueline <anginblu@gmail.com>
2018-02-01 00:09:15 -0500

    Done.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,44 @@
 class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:edit, :update, :show]
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      redirect_to @ingredient
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def edit
+  end
+
+  def update
+    @ingredient.name = params[:ingredient][:name]
+    if @ingredient.save
+      redirect_to @ingredient
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+  end
+
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,45 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:edit, :update, :show]
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def edit
+  end
+
+  def update
+    @recipe.name = params[:recipe][:name]
+    @recipe.ingredient_ids = params[:recipe][:ingredient_ids]
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, ingredient_ids: [])
+  end
+
 end
