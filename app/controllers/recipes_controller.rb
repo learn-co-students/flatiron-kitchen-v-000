acdb1ef41e1a4247cca9aa9327e104673bb1d8ac
Hoon Lee <yl.hoony@gmail.com>
2018-02-07 14:52:54 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class RecipesController < ApplicationController
 
   def index
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Recipe < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
