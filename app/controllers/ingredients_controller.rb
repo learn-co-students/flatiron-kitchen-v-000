3a41e39927cf18b0533429a0ab18ade304d25356
AnastasiaBarkett <anastasia.barkett@gmail.com>
2018-02-11 18:59:43 -0500

    Add ingredients#update action

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -8,10 +8,6 @@ class IngredientsController < ApplicationController
     @ingredient = Ingredient.new
   end
 
-  def edit
-    @ingredient = Ingredient.find(params[:id])
-  end
-
   def create
     ingredient = Ingredient.new(ingredient_params)
 
@@ -22,7 +18,25 @@ class IngredientsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @ingredient.update(ingredient_params)
+
+    if @ingredient.save
+      redirect_to ingredients_path
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
+
   def ingredient_params
     params.require(:ingredient).permit(:name)
   end
