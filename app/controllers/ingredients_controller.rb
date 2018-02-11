ef8ea1c5282a5da911d4bf1bac33985c0dc3c87b
AnastasiaBarkett <anastasia.barkett@gmail.com>
2018-02-11 18:49:59 -0500

    Create form for new and editing ingredient.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,24 @@
 class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:edit, :update]
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def create
+
+  end
+
+  private
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+
 end
