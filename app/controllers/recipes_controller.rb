277538547294d1f85830fdac78192612a117f41e
AnastasiaBarkett <anastasia.barkett@gmail.com>
2018-02-11 19:33:57 -0500

    Tests passing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,15 +20,30 @@ class RecipesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @recipe.update(recipe_params)
+
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def set_recipe
     @recipe = Recipe.find(params[:id])
   end
+
   def get_ingredients
     @ingredients = Ingredient.all
   end
+
   def recipe_params
-    params.require(:recipe).permit(:name, ingredient_ids: [])
+    params.require(:recipe).permit(:name, ingredient_ids:[])
   end
 end
