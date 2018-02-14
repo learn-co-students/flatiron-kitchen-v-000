6220d994b030ab7e699a3c75779f5ec7c0081812
bretburau <github email address>
2018-02-14 11:25:02 -0600

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,7 @@
 class RecipesController < ApplicationController
+  
   def new
+    @submit = "Create Recipe"
     @recipe = Recipe.new
   end
 
@@ -7,6 +9,18 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new(post_params)
     @recipe.save
     redirect_to recipe_path(@recipe)
+  end
+
+  def edit
+    @submit = "Update Recipe"
+    get_recipe
+  end
+
+  def update
+    get_recipe
+    @recipe.update(post_params)
+    @recipe.save
+    redirect_to @recipe
   end
 
   def show
