c44a5e2547864d7c602306a94b60d7995a49faaf
Seb Gertz <sebgertz@gmail.com>
2018-02-16 01:06:03 +0000

    edit/update

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -10,6 +10,26 @@ class IngredientsController < ApplicationController
     redirect_to ingredient_path(@ingredient)
   end
 
-  
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.update(ingredient_params)
+
+    redirect_to ingredient_path(@ingredient)
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+
 
 end
