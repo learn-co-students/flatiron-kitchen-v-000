af498ce7809934b930a87245ccbecf25160208a5
github username <github email address>
2018-02-24 10:37:59 -0500

    Done.

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,36 @@
+class IngredientsController < ApplicationController
+
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    Ingredient.create(ingredient_params)
+    redirect_to ingredients_path
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.update(ingredient_params)
+    redirect_to ingredient_path(@ingredient)
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+
+end
