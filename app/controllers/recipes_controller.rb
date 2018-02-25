34e0faadeaa4f5a9e6026bf530a675f11b0fb9ed
PJ Frias <peejfrias@gmail.com>
2018-02-25 03:32:14 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,12 +9,23 @@ class RecipesController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(@recipe)
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
   end
 
   def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
 
+  private
+
+  def recipe_params
+    params.require('recipe').permit(:name, ingredient_ids:[])
   end
 end
