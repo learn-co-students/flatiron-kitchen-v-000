73193fb60028c02373630fea228272d777e0554a
tpetersen0308 <travis.e.petersen@gmail.com>
2018-03-01 19:09:48 -0800

    Done.

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,41 @@
+class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:show, :edit, :update]
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.valid?
+      @ingredient.save
+      redirect_to ingredient_path(@ingredient)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @ingredient.valid?
+      @ingredient.update(ingredient_params)
+      redirect_to ingredient_path(@ingredient)
+    else
+      render :edit
+    end
+  end
+
+  private
+    def set_ingredient
+      @ingredient = Ingredient.find_by(:id => params[:id])
+    end
+
+    def ingredient_params
+      params.require(:ingredient).permit(:name)
+    end
+end
---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,31 @@
+class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update]
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    raise params.inspect
+    @recipe = Recipe.new(recipe_params)
+    
+    if @recipe.valid?
+      @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+    def set_recipe
+      @recipe = Recipe.find_by(:id => params[:id])  
+    end
+
+    def recipe_params
+      params.require(:recipe).permit(:name, :ingredient_ids => [], :ingredients_attributes => [:name])
+    end
+end
