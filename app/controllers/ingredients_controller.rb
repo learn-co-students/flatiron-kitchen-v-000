c2c786899f03db269e66b4c20d9df124396a29e6
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-03-04 11:28:05 -0500

    Done.

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,32 @@
+class IngredientsController < ApplicationController
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      redirect_to @ingredient
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.update(ingredient_params)
+    redirect_to ingredient_path(@ingredient)
+  end
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+end
