7efda74cc128acf636147622105f435b691e90e5
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-03-11 22:34:52 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -28,6 +28,6 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :ingredients_attributes => [:name])
+    params.require(:recipe).permit(:name, :ingredient_ids => [])
   end
 end
