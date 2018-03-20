0070a9467c7c0bacee655742aad8ed35a600f25c
funemployeddad <funemployeddad@gmail.com>
2018-03-20 16:25:51 -0700

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,14 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: [:show, :edit, :destory, :update]
+  before_action :set_recipe, only: [:show, :edit, :update]
 
     def index
       @recipes = Recipe.all
     end
 
     def show
+      if @recipe.ingredients.last.try(:name)
+        @recipe.ingredients.build
+      end
     end
 
     def new
@@ -21,14 +24,17 @@ class RecipesController < ApplicationController
       end
     end
 
-    # def edit
-    # end
-    #
-    # def update
-    # end
-    #
-    # def destroy
-    # end
+    def edit
+    end
+
+    def update
+      @recipe.update(recipe_params)
+      if @recipe.save
+        redirect_to @recipe
+      else
+        render 'edit'
+      end
+    end
 
     private
     def set_recipe
@@ -36,7 +42,7 @@ class RecipesController < ApplicationController
     end
 
     def recipe_params
-      params.require(:recipe).permit(:name)
+      params.require(:recipe).permit(:name, ingredient_ids:[])
     end
 
 end
