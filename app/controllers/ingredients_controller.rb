23db83eb2a424d640287190e8376abc62c7e2b51
jacobsilver2 <jacobsilver2@mac.com>
2018-03-28 06:38:02 -0400

    finished ingredients creation. working on updating and editing

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,35 @@
 class IngredientsController < ApplicationController
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      redirect_to @ingredient
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    
+  end
+
+
+  private
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
 end
+
+
+
