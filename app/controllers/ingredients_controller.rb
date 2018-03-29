46751fd761fb6a63db924c12beb9a2a0296f9dad
Eva Navarror <emanao@me.com>
2018-03-29 17:19:35 +0000

    generated rails structure

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,12 @@
+class IngredientsController < ApplicationController
+  def index
+  end
+  def new
+  end
+  def create
+  end
+  def edit
+  end
+  def update
+  end
+end
---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,12 @@
+class RecipesController < ApplicationController
+  def index
+  end
+  def new
+  end
+  def create
+  end
+  def edit
+  end
+  def update
+  end
+end
