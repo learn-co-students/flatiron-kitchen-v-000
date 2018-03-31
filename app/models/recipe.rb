ef84460e0beb3e52f472c96d3477408a70fcd532
Yujun Sung <yujunsung@gmail.com>
2018-03-31 22:28:17 +0000

    Done.

---
+++ app/models/recipe.rb
@@ -0,0 +1,7 @@
+class Recipe < ActiveRecord::Base
+  has_many :ingredients, through: :recipe_ingredients
+  has_many :recipe_ingredients
+  validates_presence_of :name
+  accepts_nested_attributes_for :ingredients, reject_if: proc { |attributes| attributes['name'].blank? }
+
+end
