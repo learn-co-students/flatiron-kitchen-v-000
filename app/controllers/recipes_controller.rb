2ad189dc498e536849ee5e51eddbb6922d498373
jendobes <jdobak1@gmail.com>
2018-04-11 16:40:22 +0200

    Done.

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,38 @@
+class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to recipes_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, ingredient_ids: [])
+  end
+end
